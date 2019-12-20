80c5601315fc7a570c15d46e56d9dcfc3e823117
carlos <cdbzprogr@gmail.com>
2019-12-20 12:10:53 -0500

    Initial commit, conection with now.sh

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const App = ()=>{
+    return (
+        <div>
+            It works
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
